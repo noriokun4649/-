e35e0faf929c1409f596304ce21d3169fe2cd3d0
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2020-10-26 06:47:29 +0900

    プロジェクト ファイルを追加します。

---
+++ 画像振り分け君/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ImageResizeFolder
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
