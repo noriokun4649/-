12f63226cd9255e5b38be785defba07190777b74
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2020-10-26 11:44:20 +0900

    ドロップイベントの追加

--- 画像振り分け君/MainWindow.xaml.cs
+++ 画像振り分け君/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace ImageResizeFolder
 {
@@ -10,6 +11,16 @@ namespace ImageResizeFolder
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DragBox_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void DragBox_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
         }
     }
 }
